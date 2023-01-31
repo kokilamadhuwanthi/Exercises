f326aa1fda14eaaf18887b67fc7c93e6993aa8b8
kokilamadhuwanthi <kokilamadhuwanthi67@gmail.com>
2023-01-31 21:47:17 +0530

    2019CSC022

--- java/Calc.java
+++ java/Calc.java
@@ -3,5 +3,20 @@ Problem:
 Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
 
 */
+class test1
+{
+    public static void main(String args[])
+    {
+         int x =55;
+         int y =15;
+
+        System.out.println("The sum of the number is"+x+y);
+        System.out.println("The sub of the number is"+x-y);
+        System.out.println("The multiple of the number is"+x*y);
+        System.out.println("The division of the number is"+x/y);
+        System.out.println("The remainder of the number is"+x%y);
+
+    }
+}
 
 
